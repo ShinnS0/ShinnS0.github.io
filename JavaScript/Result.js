fbb44323e842b6f0be40b3544560e4edbc07d757
ShinnS0 <zwekaung151@gmail.com>
2021-12-22 22:16:17 +0630

    assignmet js

--- JavaScript/Result.js
+++ JavaScript/Result.js
@@ -1,13 +1,36 @@
- 
-// let Result = document.getElementsByClassName(".input");
-function MyFunction() {
-   let x = document.getElementById("input").submit();
+function result(){
 
- if (x > 50) {
-     document.write('This Student is passed');
-     }else if (x <50) {
-     document.write('This student is failed.');
- }else {
-     document.write('The student is not found');
- }
+    // alert("Hello")
+    let turboc = document.myForm.turboc.value;
+    console.log(turboc);
+    let java = document.myForm.java.value;
+    console.log(java);
+    let js = document.myForm.javaScript.value;
+    console.log(js);
+
+
+    if (turboc >= 81 && java >= 81 && js >= 81) {
+
+        document.write("<h1>This student is passed with colors.</h1>");
+    
+     }else if(turboc <= 49 && (java <= 49 || js <= 49)) {
+
+        document.write("<h1>This student is failed.</h1>");
+
+    }else if (turboc <= 49 && java <= 49 && js <= 49){
+
+        document.write("<h1>This student is failed.</h1>");
+
+    }else if (turboc > 49 && java > 49 && js > 49){
+
+        document.write("<h1>This student is passed.</h1>");
+
+    }else{
+
+        document.write("</h1>This student is failed.<h1>");
+
+    }
+
 }
+
+ 
