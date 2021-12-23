308d4805db6e5a0f9f29c5d5420f90ec2f45c767
ShinnS0 <zwekaung151@gmail.com>
2021-12-23 13:30:03 +0630

    exam_result

--- JavaScript/Result.js
+++ JavaScript/Result.js
@@ -9,25 +9,42 @@ function result(){
     console.log(js);
 
 
-    if (turboc >= 81 && java >= 81 && js >= 81) {
 
-        document.write("<h1>This student is passed with colors.</h1>");
+    if(turboc <= 49 && (java <= 49 && js <= 49)) {
+
+        document.write("<h1>This student is failed.</h1>");
+
+    }else if (turboc <= 49 && (java <= 49 || js <= 49)){
+
+        document.write("<h1>This student is failed.</h1>");
+
+    }else if (turboc <= 49 || (java <= 49 || js <= 49)){
+
+        document.write("<h1>This student is failed.</h1>");
     
-     }else if(turboc <= 49 && (java <= 49 || js <= 49)) {
+    }else if (turboc >= 81 && (java >= 81 && js >= 81)) {
 
-        document.write("<h1>This student is failed.</h1>");
-
-    }else if (turboc <= 49 && java <= 49 && js <= 49){
-
-        document.write("<h1>This student is failed.</h1>");
-
-    }else if (turboc > 49 && java > 49 && js > 49){
+        document.write("This student is passed with colors.");
+    
+    }else if (turboc <= 80 && java <= 80 && js <= 80 ){
 
         document.write("<h1>This student is passed.</h1>");
 
-    }else{
+    }else if (turboc >= 81 || (java >= 81 && js >= 81)) {
 
-        document.write("</h1>This student is failed.<h1>");
+        document.write("This student is passed with colors.");
+    
+    }else if (java >= 81 || (turboc >= 81 && js >= 81)) {
+
+        document.write("This student is passed with colors.");
+    
+    }else if (js >= 81 || (turboc >= 81 && java >= 81)) {
+
+        document.write("This student is passed with colors.");
+    
+    }else {
+
+        document.write("<h1>This student is not found!.</h1>");
 
     }
 
