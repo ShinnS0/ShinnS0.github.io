cdbaf4bcece2dc2d069df33c32efb97817c8b17c
ShinnS0 <zwekaung151@gmail.com>
2022-02-20 21:55:08 +0630

    C++

---
+++ C++_class/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+int hours = mySeconds/3600;
+    int seconds = mySeconds%3600;
+    int minutes = seconds/60;
+    seconds %= 60;
---
+++ C++_class/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+string firstName;
+string lastName;
+
+int main(void)
+{
+    int a = 30;
+    cout << a;
+    cout << "Hello World!";
+    cout << "My" << "name" << "is" << "Zwe kaung kyaw";
+    
+    // return 0;
+}
